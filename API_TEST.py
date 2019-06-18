3a5c1d6e2f1c4edff3643e5d1a5b57c34bc75f2f
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-18 11:38:26 -0500

    Changed API_TEST.py

--- API_TEST.py
+++ API_TEST.py
@@ -1,3 +1,10 @@
 import requests
-input('hey')
-resp = requests.get('https://wordsapiv1.p.mashape.com/words/hey/definitions')
+def = input('Tell me a word you don\'t know')
+try:
+	int(def)
+	print ('Error, not a word')
+except:
+	resp = requests.get('https://wordsapiv1.p.mashape.com/words/%s/definitions' % def)
+	data = resp.json()
+	print(data)
+
